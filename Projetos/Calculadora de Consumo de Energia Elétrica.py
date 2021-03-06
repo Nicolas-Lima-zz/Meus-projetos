c95500346cad22a392cad7fc956bdf7e4c254b92
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-03-06 09:59:06 -0300

    Update Calculadora de Consumo de Energia Elétrica.py

--- Projetos/Calculadora de Consumo de Energia Elétrica.py
+++ Projetos/Calculadora de Consumo de Energia Elétrica.py
@@ -4,32 +4,51 @@ verde = '\033[0;32m'
 ciano = '\033[1;36m'
 final = '\033[m'
 cont = custokwh = 0
+listaaparelho = []
+listapotencia = []
+listatempo = []
+listadias = []
+listaconsumo = []
 print(f'{f" {ciano}Calculadora de Consumo de Energia Elétrica{final} ":-^84}\n')
 while True:
     if cont < 1:
         custokwh = float(input(f'Digite o custo de {vermelho}1 kWh{final} em sua região: {verde}R${final}'))
         print()
     aparelho = str(input('Nome do aparelho: '))
+    while not aparelho.isalpha():
+        aparelho = str(input('Nome do aparelho: '))
     cont += 1
-    listaaparelho = [aparelho]
-    potencia = int(input('Digite a potência do aparelho: '))
-    listapotencia = [potencia]
-    tempo = int(input('Digite o tempo de funcionamento diário do aparelho em horas: '))
+    listaaparelho.append(aparelho)
+    potencia = input('Digite a potência do aparelho: ')
+    while not potencia.isdigit():
+        potencia = input('Digite a potência do aparelho: ')
+    else:
+        potencia = int(potencia)
+    listapotencia.append(potencia)
+    tempo = input('Digite o tempo de funcionamento diário do aparelho em horas: ')
+    while not tempo.isdigit():
+        tempo = input('Digite o tempo de funcionamento diário do aparelho em horas: ')
+    else:
+        tempo = int(tempo)
     while tempo > 24:
         tempo = int(input('Digite o tempo de funcionamento diário do aparelho em horas: '))
-    listatempo = [tempo]
-    dias = int(input('Dias de uso: '))
-    listadias = [dias]
+    listatempo.append(tempo)
+    dias = input('Dias de uso: ')
+    while not dias.isdigit():
+        dias = input('Dias de uso: ')
+    else:
+        dias = int(dias)
+    listadias.append(dias)
     consumo = (potencia * tempo * dias * custokwh / 1000)
-    listaconsumo = [consumo]
+    listaconsumo.append(consumo)
     print()
     continuar = str(input('Quer calcular o consumo de outro aparelho? [S/N]: ')).upper().strip()
     while not continuar[0] in 'SN':
         continuar = str(input('Quer calcular o consumo de outro aparelho? [S/N]: ')).upper().strip()
-    if continuar.upper()[0] == 'N':
+    if continuar[0] == 'N':
         break
     print()
-for c in range(len(listapotencia)):
+for c in range(0, cont):
     print()
     print(f'O aparelho {vermelho}{listaaparelho[c]}{final} com uma potência de {listapotencia[c]} watts ligado '
           f'{listatempo[c]} horas por {listadias[c]} dias vai custar {verde}R${listaconsumo[c]:.2f}{final}')
